54665cefe7c85f24711e7e8fc6649c9107f4beea
bughh <bugphh@gmail.com>
2021-08-01 13:49:34 +0800

    绘制登录区域

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+// 按需导入，用到了哪些组件，就在这个文件中导入
+import { Button, Form, FormItem, Input } from 'element-ui'
+
+// 在 Vue 中注册这些组件
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
